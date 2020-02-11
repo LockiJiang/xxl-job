34c211398d1e93c1b2bf3ed3f28c52c01f004090
LockiJiang <love547@sina.com>
2020-02-12 00:10:29 +0800

    推送该特定版本到公司仓库

---
+++ xxl-job-sdk/src/main/java/org/xxl/job/client/XxlJobClient.java
@@ -0,0 +1,193 @@
+package org.xxl.job.client;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.xxl.job.client.model.ReturnT;
+import org.xxl.job.client.model.XxlJobInfo;
+import org.xxl.job.client.util.HttpUtil;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+public class XxlJobClient {
+	private ResourceBundle rb = ResourceBundle.getBundle("xxl_config");
+	private String webUrl = rb.getString("xxl.admin.url");
+	private int jobGroup = Integer.parseInt(rb.getString("xxl.admin.jobGroup"));
+	private String add = rb.getString("xxl.admin.api.add");
+	private String load = rb.getString("xxl.admin.api.loadJob");
+	private String update = rb.getString("xxl.admin.api.update");
+	private String delete = rb.getString("xxl.admin.api.delete");
+	private String start = rb.getString("xxl.admin.api.start");
+	private String stop = rb.getString("xxl.admin.api.stop");
+	private String log = rb.getString("xxl.admin.api.log");
+
+	/**
+	 * API调用返回值封装
+	 * 
+	 * @param isSuccess
+	 * @param msg
+	 * @return
+	 */
+	private ReturnT returns(String result) {
+		ReturnT returnT = null;
+		JSONObject object = JSONObject.parseObject(result);
+		if ("200".equals(object.getString("code"))) {
+			returnT = new ReturnT(true, object.getString("msg"), object.getString("content"));
+		} else {
+			returnT = new ReturnT(false, object.getString("msg"), object.getString("content"));
+		}
+		return returnT;
+	}
+
+	/**
+	 * 添加任务
+	 * 
+	 * @param jobInfo
+	 * @return
+	 */
+	public ReturnT createJob(XxlJobInfo jobInfo) {
+		String url = webUrl + add;
+		ReturnT returnT = null;
+		try {
+			Map<String, String> paramMap = JobToMap(jobInfo);
+			String result = new HttpUtil().post(url, paramMap);
+			returnT = returns(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			returnT = new ReturnT(false, e.getMessage(), null);
+		}
+		return returnT;
+	}
+
+	/**
+	 * 修改任务
+	 * 
+	 * @param jobInfo
+	 * @return
+	 */
+	public ReturnT updateJob(XxlJobInfo jobInfo) {
+		String url = webUrl + update;
+		ReturnT returnT = null;
+		try {
+			Map<String, String> paramMap = JobToMap(jobInfo);
+			String result = new HttpUtil().post(url, paramMap);
+			returnT = returns(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			returnT = new ReturnT(false, e.getMessage(), null);
+		}
+		return returnT;
+	}
+
+	/**
+	 * 删除任务
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public ReturnT deleteJob(String id) {
+		String url = webUrl + delete;
+		ReturnT returnT = null;
+		try {
+			Map<String, String> paramMap = new HashMap<String, String>();
+			paramMap.put("id", id);
+			String result = new HttpUtil().post(url, paramMap);
+			returnT = returns(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			returnT = new ReturnT(false, e.getMessage(), null);
+		}
+		return returnT;
+	}
+
+	/**
+	 * 查询任务
+	 * 
+	 * @param id
+	 * @return
+	 * @throws Exception
+	 */
+	public XxlJobInfo getJob(String id) {
+		String url = webUrl + load;
+		XxlJobInfo jobInfo = null;
+		try {
+			Map<String, String> paramMap = new HashMap<String, String>();
+			paramMap.put("id", id);
+			String result = new HttpUtil().post(url, paramMap);
+			jobInfo = JSON.parseObject(result, XxlJobInfo.class);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return jobInfo;
+	}
+
+	/**
+	 * 停用任务
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public ReturnT stopJob(String id) {
+		String url = webUrl + stop;
+		ReturnT returnT = null;
+		try {
+			Map<String, String> paramMap = new HashMap<String, String>();
+			paramMap.put("id", id);
+			String result = new HttpUtil().post(url, paramMap);
+			returnT = returns(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			returnT = new ReturnT(false, e.getMessage(), null);
+		}
+		return returnT;
+	}
+
+	/**
+	 * 启用任务
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public ReturnT startJob(String id) {
+		String url = webUrl + start;
+		ReturnT returnT = null;
+		try {
+			Map<String, String> paramMap = new HashMap<String, String>();
+			paramMap.put("id", id);
+			String result = new HttpUtil().post(url, paramMap);
+			returnT = returns(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			returnT = new ReturnT(false, e.getMessage(), null);
+		}
+		return returnT;
+	}
+
+	/**
+	 * 将任务参数转换为Map<String, String>作为http请求参数
+	 * 
+	 * @param jobInfo
+	 * @return
+	 */
+	private Map<String, String> JobToMap(XxlJobInfo jobInfo) {
+		Map<String, String> map = new HashMap<>();
+		if (jobInfo.getId() > 0) {
+			map.put("id", String.valueOf(jobInfo.getId()));
+		}
+		map.put("jobGroup", String.valueOf(jobGroup));
+		map.put("jobCron", jobInfo.getJobCron());
+		map.put("jobDesc", jobInfo.getJobDesc());
+		map.put("author", jobInfo.getAuthor());
+		map.put("alarmEmail", jobInfo.getAlarmEmail());
+		map.put("executorRouteStrategy", String.valueOf(jobInfo.getExecutorRouteStrategy()));
+		map.put("executorHandler", jobInfo.getExecutorHandler());
+		map.put("executorBlockStrategy", String.valueOf(jobInfo.getExecutorBlockStrategy()));
+		map.put("executorTimeout", String.valueOf(jobInfo.getExecutorTimeout()));
+		map.put("executorFailRetryCount", String.valueOf(jobInfo.getExecutorFailRetryCount()));
+		map.put("glueType", jobInfo.getGlueType());
+		map.put("executorParam", jobInfo.getExecutorParam());
+		return map;
+	}
+}
---
+++ xxl-job-sdk/src/test/java/org/xxl/job/sdk/ClientTest.java
@@ -0,0 +1,72 @@
+package org.xxl.job.sdk;
+
+import org.junit.Test;
+import org.xxl.job.client.XxlJobClient;
+import org.xxl.job.client.enums.ExecutorBlockStrategyEnum;
+import org.xxl.job.client.enums.ExecutorRouteStrategyEnum;
+import org.xxl.job.client.model.XxlJobInfo;
+
+public class ClientTest {
+	@Test
+	public void addJob() {
+		XxlJobInfo jobInfo = new XxlJobInfo();
+		jobInfo.setAlarmEmail("jiangyang003@chinssofti.com");
+		jobInfo.setAuthor("蒋洋");
+		jobInfo.setExecutorBlockStrategy(ExecutorBlockStrategyEnum.SERIAL_EXECUTION);
+		jobInfo.setExecutorFailRetryCount(0);
+		jobInfo.setExecutorHandler("procedureHandler");
+		jobInfo.setExecutorParam("{\"type\":\" DB2\",\"url\":\" jdbc:db2://10.0.0.65:50000/DATANALY\",\"user\":\"db2admin\",\"code\":\"db2admin\",\"pros\":[\"PD_ZM_K_RLIC_D\",\"PD_ZM_K_CASEINFO_D(5)\"]}");
+		jobInfo.setExecutorRouteStrategy(ExecutorRouteStrategyEnum.FIRST);
+		jobInfo.setExecutorTimeout(0);
+		jobInfo.setJobCron("0 0 12 * * ? *");
+		jobInfo.setJobDesc("system测试任务");
+		jobInfo.setJobGroup(1);
+		
+		XxlJobClient client = new XxlJobClient();
+		System.out.println(client.createJob(jobInfo));
+	}
+	
+	@Test
+	public void updateJob() {
+		XxlJobInfo jobInfo = new XxlJobInfo();
+		jobInfo.setId(4);
+		jobInfo.setAlarmEmail("jiangyang003@chinssofti.com");
+		jobInfo.setAuthor("蒋洋");
+		jobInfo.setExecutorBlockStrategy(ExecutorBlockStrategyEnum.SERIAL_EXECUTION);
+		jobInfo.setExecutorFailRetryCount(0);
+		jobInfo.setExecutorHandler("procedureHandler");
+		jobInfo.setExecutorParam("{\"type\":\"DB2\",\"url\":\"jdbc:db2://10.0.0.65:50000/DATANALY\",\"user\":\"db2admin\",\"code\":\"db2admin\",\"pros\":[\"PD_ZM_K_RLIC_D\",\"PD_ZM_K_CASEINFO_D(5)\"]}");
+		jobInfo.setExecutorRouteStrategy(ExecutorRouteStrategyEnum.FIRST);
+		jobInfo.setExecutorTimeout(0);
+		jobInfo.setJobCron("0 0/2 * * * ? *");
+		jobInfo.setJobDesc("system测试任务");
+		jobInfo.setJobGroup(1);
+		
+		XxlJobClient client = new XxlJobClient();
+		System.out.println(client.updateJob(jobInfo));
+	}
+	
+	@Test
+	public void startJob() {
+		XxlJobClient client = new XxlJobClient();
+		System.out.println(client.startJob(4 + ""));
+	}
+	
+	@Test
+	public void stopJob() {
+		XxlJobClient client = new XxlJobClient();
+		System.out.println(client.stopJob(4 + ""));
+	}
+	
+	@Test
+	public void removeJob() {
+		XxlJobClient client = new XxlJobClient();
+		System.out.println(client.deleteJob(7 + ""));
+	}
+	
+	@Test
+	public void loadJob() {
+		XxlJobClient client = new XxlJobClient();
+		System.out.println(client.getJob(4 + ""));
+	}
+}
